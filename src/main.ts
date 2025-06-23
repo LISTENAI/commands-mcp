e33e3e18c95fdbdb6506131e7613e81cea287203
xychen <xychen@listenai.com>
2025-06-23 10:42:03 +0800

    打印 CWD

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import type { NestExpressApplication } from '@nestjs/platform-express';
 import type { AddressInfo } from 'net';
 
 import { AppModule } from '@/app.module';
-import { OPTIONS_TOKEN, type Options } from '@/args.module';
+import { OPTIONS_TOKEN, WORKING_DIRECTORY_TOKEN, type Options } from '@/args.module';
 
 const app = await NestFactory.create<NestExpressApplication>(AppModule, {
   logger: false,
 });
 
 const opts = app.get<Options>(OPTIONS_TOKEN);
+const cwd = app.get<string>(WORKING_DIRECTORY_TOKEN);
 
 if (opts.verbose) {
   app.useLogger(new ConsoleLogger());
@@ -25,3 +26,5 @@ if (opts.stdio) {
   const server = await app.listen(opts.port ?? 0);
   console.error(`MCP Server is running at http://localhost:${(server.address() as AddressInfo).port}/sse`);
 }
+
+console.error(`Working directory: ${cwd}`);
